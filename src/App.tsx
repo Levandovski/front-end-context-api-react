6d1dc6a9f4e94fb5d16bdee6247310ef07612a4f
Jesse Levandovski <961151@lojasrenner.com.br>
2023-01-09 11:27:52 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect, useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+import { Login } from "./pages/login";
+import { AuthContext } from "./providers/auth";
+import { NameProps } from "./providers/interface";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route key={0} path='/login' element={<Login />} />
+        <Route key={1} path='/dashboard' element={<Dashboard />} />
+        <Route path='*' element={<Navigate to='/login' />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+
